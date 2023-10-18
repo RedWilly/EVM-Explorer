5136c61bdb2dde3014e5e0eeb8f9b028b21308ce
RedWilly <106310233+RedWilly@users.noreply.github.com>
2023-10-18 04:08:52 -0400

    Update dbApi.js

--- frontend/src/redux/dbApi.js
+++ frontend/src/redux/dbApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const dbApi = axios.create({
-    baseURL: "https://backend-wjjy.onrender.com",
+    baseURL: "https://backend-wjjy.onrender.com", //localhost:3001
     //baseURL : "https://dev-explorer-server.gen.foundation",
     headers: { 'content-type': "application/json" }
 })
